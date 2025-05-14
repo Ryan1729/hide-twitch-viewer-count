24fe92b11bf8ac5bb156e05cf99db2bd67403bb6
Ryan1729 <Ryan1729@gmail.com>
2025-05-14 01:06:00 -0600

    hide viewer counts on the left sidebar too

--- hide-twitch-viewer-count.js
+++ hide-twitch-viewer-count.js
@@ -1,5 +1,48 @@
-const viewerCounts = document.querySelectorAll('[data-a-target="animated-channel-viewers-count"]');
+//
+// Utils
+//
 
-for (let i = 0; i < viewerCounts.length; i += 1) {
-    viewerCounts[i].style.display = 'none';
+const numberRegex = /\p{Number}/gu;
+
+const hideAllNodesStartingWithNumerals = (root) => {
+    const childNodes = root.childNodes;
+    for (let i = 0; i < childNodes.length; i += 1) {
+        const node = childNodes[i]
+        if (node.nodeType == 3 /* Text */) {
+            const firstChar = node.nodeValue[0]
+            if (firstChar.match(numberRegex)) {
+                // Hide the whole parent node
+                root.style.display = 'none';
+            }
+        }
+
+        hideAllNodesStartingWithNumerals(node)
+    }
 }
+
+//
+// Hiding Specific Sections
+//
+
+const hideUnderStream = () => {
+    const viewerCounts = document.querySelectorAll('[data-a-target="animated-channel-viewers-count"]');
+
+    for (let i = 0; i < viewerCounts.length; i += 1) {
+        viewerCounts[i].style.display = 'none';
+    }
+};
+
+const hideLeftSidebar = () => {
+    const statusSections = document.querySelectorAll('[data-a-target="side-nav-live-status"]');
+
+    for (let i = 0; i < statusSections.length; i += 1) {
+        hideAllNodesStartingWithNumerals(statusSections[i]);
+    }
+};
+
+//
+// Trigger Hiding
+//
+
+hideUnderStream();
+hideLeftSidebar();
